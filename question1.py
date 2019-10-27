c14f3c399b36d3a8bc673d3e1653c91127c6794c
Andrew Selzler <aselzler3@Andrews-MacBook-Pro.local>
2019-10-26 21:38:13 -0700

    test questions

---
+++ question1.py
@@ -0,0 +1,10 @@
+"""
+Complete the function xsquaredplus1 below. It should take any number x as input and return the
+value of x^2 + 1. Only change the code in the specified area. Do NOT write modify any part of this
+file outside of the specified area to write the code.
+"""
+
+def xsquaredplus1(x):
+    ###########YOUR CODE HERE#############
+    pass
+    ############END OF CODE###############
---
+++ question2.py
@@ -0,0 +1,11 @@
+"""
+Complete the function lenlist below. It should take any list l as input and return the string "Long"
+if the length of the string is higher than 10, otherwise return the string "Short". Only change the
+code in the specified area. Do NOT write modify any part of this file outside of the specified area
+to write the code.
+"""
+
+def lenlist(l):
+    ###########YOUR CODE HERE#############
+    pass
+    ############END OF CODE###############
